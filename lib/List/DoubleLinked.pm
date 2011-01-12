bbdf410716f09675277310834e2690a44b6d7b12
Leon Timmermans <fawaka@gmail.com>
2011-01-12 16:17:28 +0100

    Added various methods
    Added front, back, empty, size, insert_before, insert_after, erase

--- lib/List/DoubleLinked.pm
+++ lib/List/DoubleLinked.pm
@@ -1,6 +1,10 @@
 package List::DoubleLinked;
+
 use strict;
 use warnings;
+
+use Carp qw/carp/;
+use Scalar::Util 'weaken';
 
 sub new {
 	my ($class, @items) = @_;
@@ -71,6 +75,76 @@ sub flatten {
 	return @ret;
 }
 
+sub front {
+	my $self = CORE::shift;
+	return $self->{head} ? $self->{head}{item} : undef;
+}
+sub back {
+	my $self = CORE::shift;
+	return $self->{tail} ? $self->{tail}{item} : undef;
+}
+
+sub empty {
+	my $self = CORE::shift;
+	return not defined $self->{head};
+}
+sub size {
+	my $self = CORE::shift;
+	my $ret = 0;
+	for (my $current = $self->{head}; $current; $current = $current->{next}) {
+		$ret++;
+	}
+	return $ret;
+}
+
+sub insert_before {
+	my ($self, $iter, @items) = @_;
+	my $node = $iter->[0];
+	for my $item (@items) {
+		my $new_node = {
+			item => $item,
+			prev => $node->{prev},
+			next => $node,
+		};
+		$node->{prev}{next} = $new_node if $node->{prev};
+		$node->{prev} = $new_node;
+
+		$self->{head} = $node->{next} if $self->{head} and $self->{head} == $node;
+	}
+}
+
+sub insert_after {
+	my ($self, $iter, @items) = @_;
+	my $node = $iter->[0];
+	for my $item (@items) {
+		my $new_node = {
+			item => $item,
+			prev => $node,
+			next => $node->{next},
+		};
+		$node->{next}{prev} = $new_node if $node->{next};
+		$node->{next} = $new_node;
+
+		$self->{tail} = $new_node if $self->{tail} and $self->{tail} == $node;
+		$node = $new_node;
+	}
+}
+
+sub erase {
+	my ($self, $node) = @_;
+
+	$node->{prev}{next} = $node->{next} if $node->{prev};
+	$node->{next}{prev} = $node->{prev} if $node->{next};
+
+	$self->{head} = $node->{next} if $self->{head} and $self->{head} == $node;
+	$self->{tail} = $node->{previous} if $self->{tail} and $self->{tail} == $node;
+
+	weaken $node;
+	carp 'Node may be leaking' if $node;
+
+	return;
+}
+
 sub begin {
 	my $self = CORE::shift;
 	return List::DoubleLinked::Iterator->new($self, $self->{head});
@@ -81,9 +155,19 @@ sub end {
 	return List::DoubleLinked::Iterator->new($self->{tail});
 }
 
+sub DESTROY {
+	my $self = CORE::shift;
+	my $current = $self->{head};
+	while ($current) {
+		delete $current->{prev};
+		$current = delete $current->{next};
+	}
+	return;
+}
+
 package List::DoubleLinked::Iterator;
 
-use Carp qw/croak carp/;
+use Carp qw/croak/;
 use Scalar::Util 'weaken';
 
 sub new {
@@ -110,25 +194,31 @@ sub previous {
 	my $self = shift;
 	my ($node, $list) = @{$self};
 	croak 'Node no longer exists' if not defined $node;
-	return __PACKAGE__->new($list, $node->{previous});
+	return __PACKAGE__->new($list, $node->{prev});
 }
 
 sub remove {
 	my $self = shift;
 	my ($node, $list) = @{$self};
-	return if not defined $node;
-
-	$node->{prev}{next} = $node->{next} if $node->{prev};
-	$node->{next}{prev} = $node->{prev} if $node->{next};
-
-	$list->{head} = $node->{next} if $list->{head} and $list->{head} == $node;
-	$list->{tail} = $node->{previous} if $list->{tail} and $list->{tail} == $node;
+	die if not defined $node;
 
 	my $item = $node->{item};
 	weaken $node;
-	carp 'Node may be leaking' if $node;
+	$list->erase($node);
 
 	return $item;
+}
+
+sub insert_before {
+	my ($self, @items) = @_;
+	my ($node, $list) = @{$self};
+	return $list->insert_before($self, @items);
+}
+
+sub insert_after {
+	my ($self, @items) = @_;
+	my ($node, $list) = @{$self};
+	return $list->insert_after($self, @items);
 }
 
 # ABSTRACT: Double Linked Lists for Perl
--- t/10-basics.t
+++ t/10-basics.t
@@ -3,15 +3,26 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Test::More;
+use Test::More tests => 19;
 use Test::Differences;
 use List::DoubleLinked;
+use Scalar::Util qw/weaken/;
 
 my $list = List::DoubleLinked->new;
 
 eq_or_diff [$list->flatten], [], 'List is empty';
 
+ok $list->empty;
+
+is $list->size, 0;
+
 $list->push(qw/foo bar baz/);
+
+ok !$list->empty;
+
+is $list->size, 3;
+
+is $list->front, 'foo';
 
 eq_or_diff [$list->flatten], [qw/foo bar baz/], 'List has three members';
 
@@ -23,16 +34,35 @@ $list->unshift('quz');
 
 eq_or_diff([ $list->flatten ], [ qw/quz foo bar/ ]);
 
-my $iter = $list->begin;
+{
+	my $iter = $list->begin;
 
-is $iter->get, 'quz';
+	is $iter->get, 'quz';
 
-$iter = $iter->next;
+	$iter = $iter->next;
 
-is $iter->get, 'foo';
+	is $iter->get, 'foo';
 
-is $iter->remove(), 'foo';
+	$iter->insert_before(qw/FOO BAR/);
 
-eq_or_diff([ $list->flatten ], [ qw/quz bar/ ]);
+	is $iter->get, 'foo';
 
-done_testing;
+	eq_or_diff([ $list->flatten ], [ qw/quz FOO BAR foo bar/ ]);
+
+	is $iter->previous->remove(), 'BAR';
+
+	eq_or_diff([ $list->flatten ], [ qw/quz FOO foo bar/ ]);
+
+	$iter->insert_after(qw/BUZ QUZ/);
+
+	eq_or_diff([ $list->flatten ], [ qw/quz FOO foo BUZ QUZ bar/ ]);
+
+	is $list->back, 'bar';
+
+}
+
+my $ref = $list->{head};
+weaken $ref;
+undef $list;
+ok !defined $ref, 'ref should no longer be defined';
+
